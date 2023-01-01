d7b8c7b3bb20d931ca3f1ebb3147dc91a9f4ff06
Gilinsik <lotus0110@nate.com>
2023-01-01 15:41:50 +0900

    feat : add schema user, group, feed

--- server/prisma/migrations/20221230142826_create_feeds_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Feed` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `url` VARCHAR(191) NOT NULL,
-    `content` VARCHAR(191) NULL,
-    `alarm` BOOLEAN NULL DEFAULT true,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
