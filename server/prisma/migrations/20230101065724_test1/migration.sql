1935d7b6375d3d27b1b0c37f49581cecd7f0e844
Gilinsik <lotus0110@nate.com>
2023-01-01 15:58:09 +0900

    feat : add testfeed schema

---
+++ server/prisma/migrations/20230101065724_test1/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `TestFeed` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `url` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NULL,
+    `group` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
