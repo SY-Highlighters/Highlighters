7bfa68a4b1cb8b06a5401e7d99ce4769c0c09c1c
Gilinsik <lotus0110@nate.com>
2023-01-01 16:35:49 +0900

    feat : enablecors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
