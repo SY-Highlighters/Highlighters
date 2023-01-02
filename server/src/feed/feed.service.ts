cd54c7f3fe340cb3cb150803efb48297970c66a3
Gilinsik <lotus0110@nate.com>
2023-01-02 22:23:32 +0900

    feat : import cheerio

--- server/src/feed/feed.service.ts
+++ server/src/feed/feed.service.ts
@@ -1,3 +1,4 @@
+import { Cheerio } from './../../node_modules/cheerio/lib/cheerio.d';
 import {
   JsonObject,
   JsonArray,
@@ -8,7 +9,6 @@ import { TestFeed } from '.prisma/client';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/repository/prisma.service';
 import { TestFeedRequestDto } from './dto/testfeed.request';
-import { getUrlMeta } from 'src/utils/geturlmeta';
 
 @Injectable()
 export class FeedService {
@@ -30,32 +30,125 @@ export class FeedService {
   // 그룹아이디에 따른 피드 조회
   async fetchFeedByGroupId(id: number): Promise<object[] | null> {
     // feeds 받기
-    const feeds: JsonArray = await this.prismaService
+    const feeds: Feed[] = await this.prismaService
       .$queryRaw`SELECT * FROM FEED WHERE group_id = ${id}`;
     const feedswithOg: object[] = [];
-    console.log(typeof feeds);
-    feeds.map(async (feed: JsonObject) => {
-      // const getfeed: Feed = JSON.parse(feed.toString());
+    // console.log(feeds);
+
+    // http meta data 가져오기
+    for (const feed of feeds) {
       // url이 있으면 메타데이터 가져오기
-      if (feed.URL) {
-        console.log(feed.URL);
-        const meta = await getUrlMeta(feed.URL);
+      console.log(feed);
+      if (feed.url) {
+        console.log(feed.url);
+        const meta = await this.getUrlMeta(feed.url);
         const feedwithOg = {
-          feed_id: feed.FEED_ID,
-          group_id: feed.GROUP_ID,
-          url: feed.URL,
-          createdAt: feed.CREATEDAT,
-          updatedAt: feed.UPADATEDAT,
-          user_id: feed.USER_ID,
+          feed_id: feed.id,
+          group_id: feed.group_id,
+          url: feed.url,
+          createdAt: feed.createdAt,
+          updatedAt: feed.updatedAt,
+          user_id: feed.user_email,
           og_title: meta.title,
-          og_desc: meta.desc,
+          og_desc: meta.description,
           og_image: meta.image,
         };
         feedswithOg.push(feedwithOg);
+        console.log(feedswithOg);
       }
-    });
+    }
     return feedswithOg;
   }
+
+  async _getHostname(url) {
+    let start = url.indexOf('://') + 3;
+    let end = url.indexOf('/', start);
+    return url.slice(start, end);
+  }
+
+  async _getProtocol(url) {
+    let end = url.indexOf('://') + 3;
+    return url.slice(0, end);
+  }
+
+  async _bodyScrap(url) {
+    return ($) => {
+      // 글제목
+      let title = $("meta[property='og:title']").attr('content');
+      if (!title) {
+        title = $('head title').text();
+        if (!title) {
+          throw Error('This link has no title');
+        }
+      }
+      // 글이미지
+      let image = $("meta[property='og:image']").attr('content');
+      if (!image) {
+        image = $('img').attr('src');
+        //이미지 세팅
+        if (image && image.indexOf('http') === 0) {
+          // http 로 시작하면 그냥 사용
+        } else if (image && image[0] === '/') {
+          // image 경로가 / 로 시작한다면
+          //let urlObj = new URL(url);
+          image =
+            'this._getProtocol(url)' + 'this._getHostname(url)' + $(image);
+        } else {
+          image = '';
+        }
+      }
+
+      // 글요약본
+      let desc = $("meta[property='og:description']").attr('content');
+      if (!desc) {
+        desc = '';
+      }
+      return {
+        title,
+        image,
+        desc,
+      };
+    };
+  }
+
+  async getUrlMeta(url) {
+    const meta = await fetch(url)
+      .then((res) => res.text())
+      .then((body) => {
+        const $ = Cheerio.load(body);
+        return this._bodyScrap(url)($);
+      })
+      .catch((err) => {
+        console.log(err);
+        return {
+          title: '',
+          image: '',
+          desc: '',
+        };
+      });
+    return meta;
+  }
+
+  // feeds.map(async (feed: JsonObject) => {
+  //   // const getfeed: Feed = JSON.parse(feed.toString());
+  //   // url이 있으면 메타데이터 가져오기
+  //   if (feed.URL) {
+  //     console.log(feed.URL);
+  //     const meta = await getUrlMeta(feed.URL);
+  //     const feedwithOg = {
+  //       feed_id: feed.FEED_ID,
+  //       group_id: feed.GROUP_ID,
+  //       url: feed.URL,
+  //       createdAt: feed.CREATEDAT,
+  //       updatedAt: feed.UPADATEDAT,
+  //       user_id: feed.USER_ID,
+  //       og_title: meta.title,
+  //       og_desc: meta.desc,
+  //       og_image: meta.image,
+  //     };
+  //     feedswithOg.push(feedwithOg);
+  //   }
+  // });
 
   // 유저아이디, 그룹아이디에 따른 피드 생성
   async createFeed(body: FeedRequestDto): Promise<Feed> {
