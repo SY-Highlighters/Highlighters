04a4f2d8bd717b3ca6cf5a95732dc56819768002
Gilinsik <lotus0110@nate.com>
2023-01-02 21:30:17 +0900

    js 파일 고침1

--- server/src/highlight/dto/create-highlight.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.CreateHighlightDto = void 0;
-var CreateHighlightDto = /** @class */ (function () {
-    function CreateHighlightDto() {
-    }
-    return CreateHighlightDto;
-}());
-exports.CreateHighlightDto = CreateHighlightDto;
--- server/src/highlight/dto/update-highlight.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.UpdateHighlightDto = void 0;
-var UpdateHighlightDto = /** @class */ (function () {
-    function UpdateHighlightDto() {
-    }
-    return UpdateHighlightDto;
-}());
-exports.UpdateHighlightDto = UpdateHighlightDto;
