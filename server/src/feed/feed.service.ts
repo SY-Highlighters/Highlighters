c4476bc684dad601ecbae3594f880dcdbeedfec6
yeriiiii <yerina98@gmail.com>
2023-01-02 23:11:46 +0900

    fix: feed meta

--- server/src/feed/feed.service.ts
+++ server/src/feed/feed.service.ts
@@ -9,6 +9,7 @@ import { TestFeed } from '.prisma/client';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/repository/prisma.service';
 import { TestFeedRequestDto } from './dto/testfeed.request';
+import { getUrlMeta } from 'src/util/geturlmeta';
 
 @Injectable()
 export class FeedService {
@@ -28,11 +29,38 @@ export class FeedService {
   }
 
   // 그룹아이디에 따른 피드 조회
-  async fetchFeedByGroupId(id: number): Promise<Feed[]> {
-    const feeds = await this.prismaService.feed.findMany({
-      where: { group_id: id },
-    });
-    return feeds;
+
+  async fetchFeedByGroupId(id: number): Promise<object[] | null> {
+    // feeds 받기
+    const feeds: Feed[] = await this.prismaService
+      .$queryRaw`SELECT * FROM FEED WHERE group_id = ${id}`;
+    const feedswithOg: object[] = [];
+    // console.log(feeds);
+
+    // http meta data 가져오기
+    for (const feed of feeds) {
+      // url이 있으면 메타데이터 가져오기
+      // console.log(feed);
+      if (feed.url) {
+        console.log(feed.url);
+        const meta = await getUrlMeta(feed.url);
+        // console.log(meta);
+        const feedwithOg = {
+          feed_id: feed.id,
+          group_id: feed.group_id,
+          url: feed.url,
+          createdAt: feed.createdAt,
+          updatedAt: feed.updatedAt,
+          user_id: feed.user_email,
+          og_title: meta.title,
+          og_desc: meta.desc,
+          og_image: meta.image,
+        };
+        console.log(feedswithOg);
+        feedswithOg.push(feedwithOg);
+      }
+    }
+    return feedswithOg;
   }
 
   // 유저아이디, 그룹아이디에 따른 피드 생성
