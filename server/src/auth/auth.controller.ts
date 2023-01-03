6fa3f56c0f887534e1a8af1fbdc1e3339772ecd1
chohon <siaksiakx@gmail.com>
2023-01-03 11:46:38 +0900

    chore : prettier

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,21 +1,26 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Post } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { AuthService } from './auth.service';
-import { AuthSigninCredentialsDto, AuthSignupCredentialsDto } from './dto/auth.credential.dto';
+import {
+  AuthSigninCredentialsDto,
+  AuthSignupCredentialsDto,
+} from './dto/auth.credential.dto';
 
 @Controller('api/auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
-    @Post('/signup')
-    signUp(@Body() authSignupCredentialsDto: AuthSignupCredentialsDto): Promise<User> {
-        return this.authService.signUp(authSignupCredentialsDto);
-    }
+  @Post('/signup')
+  signUp(
+    @Body() authSignupCredentialsDto: AuthSignupCredentialsDto,
+  ): Promise<User> {
+    return this.authService.signUp(authSignupCredentialsDto);
+  }
 
-    @Post('/signin')
-    signIn(@Body() authSigninCredentialsDto: AuthSigninCredentialsDto): Promise<{accessToken: string}> {
-        return this.authService.signIn(authSigninCredentialsDto);
-    }
-
+  @Post('/signin')
+  signIn(
+    @Body() authSigninCredentialsDto: AuthSigninCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authSigninCredentialsDto);
+  }
 }
