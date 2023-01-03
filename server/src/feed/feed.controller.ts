55ebd99229e7a4a4585c246eb45b9c6385706b90
devsungtae <devsungtae@gmail.com>
2023-01-03 18:03:27 +0900

    google login 구현 완료

--- server/src/feed/feed.controller.ts
+++ server/src/feed/feed.controller.ts
@@ -1,12 +1,13 @@
 import { FeedService } from './feed.service';
 import { Controller, Get, Post, Delete } from '@nestjs/common';
-import { Feed } from '@prisma/client';
+import { Feed, User } from '@prisma/client';
 import { Body, Param, UseGuards } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateFeedDto } from './dto/feed.dto';
+import { GetUser } from 'src/auth/get-user.decorator';
 
 @Controller('api/feed')
-// @UseGuards(AuthGuard())
+@UseGuards(AuthGuard())
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
 
@@ -15,7 +16,7 @@ export class FeedController {
     return this.feedService.createFeed(createFeedDto);
   }
 
-  @Get('/:id')
+  @Get('/id/:id')
   async findFeedById(@Param('id') id: number): Promise<Feed> {
     return this.feedService.findFeedById(id);
   }
@@ -29,4 +30,11 @@ export class FeedController {
   async deleteFeedById(@Param('id') id: number): Promise<Feed> {
     return this.feedService.deleteFeedById(id);
   }
+
+  // @Get('/getusertest/')
+  // getuser(@GetUser() user: User): Promise<User> { // 
+  //   console.log('getuser 들어왔다')
+  //   console.log('user', user.email, user.group_id)
+  //   return 
+  // }
 }
