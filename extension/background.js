d7902bd92b1d8de60ff7c4eb8563c075d63c6709
yeriiiii <yerina98@gmail.com>
2023-01-07 04:01:05 +0900

    fix : background 이식수술 성공

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,36 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  
-})
-
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // 웹페이지의 하이라이팅을 디비로 전송
+  if (request.greeting === "posthighlight") {
+    console.log("[background] request:" + request.greeting);
+    fetch("http://localhost:3001/api/highlight/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request.data),
+    }).then((response) => {
+      sendResponse({ farewell: response });
+    });
+    return true;
+  }
+  // 웹페이지의 모든 하이라이트를 불러옴
+  else if (request.greeting === "gethighlight") {
+    console.log(request.data);
+    fetch("http://localhost:3001/api/highlight/feed", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request.data),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("gethighlight in bg", data);
+        sendResponse({ data: data });
+      })
+      .catch((error) => console.log("gethighlight in bg : none"));
+    return true;
+  }
+});
