5ce77ef67fafe20dd71e086141e78284cff06952
Gilinsik <lotus0110@nate.com>
2023-01-08 23:35:51 +0900

    feat : calendar

--- server/src/calendar/calendar.service.ts
+++ server/src/calendar/calendar.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/repository/prisma.service';
 
 @Injectable()
-export class CalendarService {}
+export class CalendarService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async showCalendar(user: User, date: Date): Promise<object[]> {
+    const calendar = await this.prisma.feed.findMany({
+      where: {
+        group_id: user.group_id,
+        createdAt: {
+          gte: new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+        },
+      },
+      orderBy: { createdAt: 'desc' },
+      include: {
+        highlight: true,
+        tag: true,
+      },
+    });
+
+    return calendar;
+  }
+}
