6152455cb71ca834480ddca94d85214aaecc8581
Gilinsik <lotus0110@nate.com>
2023-01-08 14:03:18 +0900

    feat : create tag

--- server/src/tag/tag.controller.ts
+++ server/src/tag/tag.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseFilters,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptors';
+import { CreateTagDto } from './dto/tag.dto';
+import { TagService } from './tag.service';
 
-@Controller('tag')
-export class TagController {}
+@Controller('/api/tag')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
+@UseGuards(AuthGuard())
+export class TagController {
+  constructor(private readonly tagService: TagService) {}
+
+  // 태그 생성
+  @Post('/create')
+  async createTag(
+    @Body() createTagDto: CreateTagDto,
+    @GetUser() user: User,
+  ): Promise<null> {
+    createTagDto.group_id = user.group_id;
+
+    return this.tagService.createTag(createTagDto);
+  }
+
+  // 태그 삭제
+
+  // 태그 조회(웹)
+}
--- server/src/tag/tag.service.ts
+++ server/src/tag/tag.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
+import { PrismaService } from 'src/repository/prisma.service';
+import { CreateTagDto } from './dto/tag.dto';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createTag(createTagDto: CreateTagDto): Promise<null> {
+    const { tag_name, feed_id, group_id } = createTagDto;
+    try {
+      await this.prismaService.tag.create({
+        data: {
+          tag_name: tag_name,
+          feed: {
+            connect: {
+              id: feed_id,
+            },
+          },
+          group: {
+            connect: {
+              id: group_id,
+            },
+          },
+        },
+      });
+    } catch (e) {
+      console.log(e);
+      throw new HttpException('Internal Server Error', 500);
+    }
+
+    return null;
+  }
+}
