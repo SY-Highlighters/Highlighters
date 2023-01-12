a2f074fbdae89cc7959aff14b4b5cd7c9dfb8e2f
Gilinsik <lotus0110@nate.com>
2023-01-12 17:11:39 +0900

    feat : find sep feed by id

--- server/src/feed/feed.controller.ts
+++ server/src/feed/feed.controller.ts
@@ -1,3 +1,4 @@
+import { GetUser } from './../auth/get-user.decorator';
 import { ApiResponse, ApiOperation } from '@nestjs/swagger';
 import { FeedService } from './feed.service';
 import { Controller, Get, Post, Delete } from '@nestjs/common';
@@ -8,10 +9,10 @@ import {
   UseFilters,
   UseGuards,
   UseInterceptors,
+  Query,
 } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateFeedDto } from './dto/feed.dto';
-import { GetUser } from 'src/auth/get-user.decorator';
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptors';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 
@@ -43,6 +44,18 @@ export class FeedController {
     createFeedDto.group_id = user.group_id;
 
     return this.feedService.createFeed(createFeedDto);
+  }
+
+  // Feed take 개의 page Feed 반환
+  @ApiResponse({ status: 200, description: 'success', type: 'Feed' })
+  @ApiOperation({ summary: 'Feed take 개의 page Feed 반환' })
+  @Get('/sep/feed')
+  async findSepFeedById(
+    @Query('page') page: number,
+    @Query('take') take: number,
+    @GetUser() user: User,
+  ) {
+    return this.feedService.findSepFeedById(Number(page), Number(take), user);
   }
 
   // Id로 Feed 찾기
