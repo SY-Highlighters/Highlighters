6aeecc1f4ab1edd0fd2ff0e17c6723ac5d927aa8
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-01-12 23:00:53 +0900

    feat:extension tag add and enter press handler

--- extension/src/tabs/CreateFeed.tsx
+++ extension/src/tabs/CreateFeed.tsx
@@ -5,15 +5,11 @@ import { useSetRecoilState, useRecoilState } from "recoil";
 import { tagsInFeedState } from "../states/atom";
 import TagData from "../models/tag";
 
-
-
-
 export default function CreateFeed(this: any) {
   const [groupTags, setGroupTags] = useState<TagData[]>([]);
   const [feedTags, setFeedTags] = useRecoilState(tagsInFeedState);
-
+  const [tagInput, setTagInput] = useState<string>("");
   useEffect(() => {
-
     async function getTagAsync() {
       await chrome.storage.sync.set({ highlightColor: "#FFFF00" });
       const color = await chrome.storage.sync.get("highlightColor");
@@ -38,7 +34,29 @@ export default function CreateFeed(this: any) {
     });
   };
 
+  const tagInputChangeHandler = (event: any) => {
+    setTagInput(event.target.value);
+  };
+  const tagAddHandler = () => {
+    // 서버에 api 요청
+    const newTag = {
+      tag_id: tagInput,
+      tag_name: tagInput,
+    };
 
+    feedTags.push(newTag);
+    setFeedTags(feedTags);
+    setTagInput("");
+  };
+
+  // enter event handler
+  const activeEnter = (e: any) => {
+    console.log(e);
+    if (e.key === "Enter") {
+      console.log("enter");
+      tagAddHandler();
+    }
+  };
   const groupTagList = groupTags.map((tag) => (
     <span>
       <TagItem
@@ -49,59 +67,64 @@ export default function CreateFeed(this: any) {
     </span>
   ));
 
-
   return (
     <div>
       <div className="px-4"></div>
       {/* <form action="#" method="POST"> */}
-        <div className="shadow ">
-          <div className="bg-white px-4 py-3">
-            <h1 className="text-base font-semibold text-left text-sky-500 ">
-              피드 제목
-            </h1>
-            <input
-              type="text"
-              className="w-full h-8 mr-1 mb-1 mt-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-gray-400"
-              placeholder=" 제목을 입력하세요"
-            />
-            <h1 className="my-1 text-base font-semibold text-left text-sky-500 ">
-              태그 추가
-            </h1>
-            <div className="mb-2">
-              {feedTags.map((tag: any) => (
-                <span>
-                  <TagItem id={tag.tag_id} name={tag.tag_name}></TagItem>
-                </span>
-              ))}
-            </div>
-            <div className="items-center w-full px-3 py-2 text-gray-700 bg-white border border-gray-300 rounded-lg focus:outline-none focus:border-gray-400">
-              <div className="flex flex-row">
-                <div className="w-full">
-                  <input
-                    type="text"
-                    className="w-72 h-8 mr-1 mb-1 mt-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-gray-400"
-                    placeholder=" 태그를 입력하세요"
-                  />
-                </div>
-                <button className="flex flex-row items-center justify-between w-8 h-8 ml-1 mb-1 mt-1 text-gray-400 bg-white  border-white rounded-lg focus:outline-none focus:border-gray-400">
-                  <ArrowRightCircleIcon
-                    className="w-8 h-8 hover:text-sky-500"
-                    aria-hidden="true"
-                  />
-                </button>
-              </div>
-              <div>{groupTagList}</div>
-            </div>
+      <div className="shadow ">
+        <div className="px-4 py-3 bg-white">
+          <h1 className="text-base font-semibold text-left text-sky-500 ">
+            피드 제목
+          </h1>
+          <input
+            type="text"
+            className="w-full h-8 mt-1 mb-1 mr-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-gray-400"
+            placeholder=" 제목을 입력하세요"
+          />
+          <h1 className="my-1 text-base font-semibold text-left text-sky-500 ">
+            태그 추가
+          </h1>
+          <div className="mb-2">
+            {feedTags.map((tag: any) => (
+              <span>
+                <TagItem id={tag.tag_id} name={tag.tag_name}></TagItem>
+              </span>
+            ))}
           </div>
-          <div className="bg-gray-50 px-4 py-2 text-right">
-            <button
-              type="submit"
-              className="inline-flex justify-center rounded-md border border-transparent bg-sky-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
-            >
-              Submit
-            </button>
+          <div className="items-center w-full px-3 py-2 text-gray-700 bg-white border border-gray-300 rounded-lg focus:outline-none focus:border-gray-400">
+            <div className="flex flex-row">
+              <div className="w-full">
+                <input
+                  onChange={tagInputChangeHandler}
+                  type="text"
+                  className="h-8 mt-1 mb-1 mr-1 text-gray-700 bg-white border border-gray-300 rounded-md w-72 focus:outline-none focus:border-gray-400"
+                  placeholder="태그를 입력하세요"
+                  value={tagInput}
+                  onKeyDown={activeEnter}
+                />
+              </div>
+              <button
+                onClick={tagAddHandler}
+                className="flex flex-row items-center justify-between w-8 h-8 mt-1 mb-1 ml-1 text-gray-400 bg-white border-white rounded-lg focus:outline-none focus:border-gray-400"
+              >
+                <ArrowRightCircleIcon
+                  className="w-8 h-8 hover:text-sky-500"
+                  aria-hidden="true"
+                />
+              </button>
+            </div>
+            <div>{groupTagList}</div>
           </div>
         </div>
+        <div className="px-4 py-2 text-right bg-gray-50">
+          <button
+            type="submit"
+            className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-sky-500 hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
       {/* </form> */}
     </div>
   );
