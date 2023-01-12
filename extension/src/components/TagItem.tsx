7ab76145692dd37b23689f1e465756937ad25894
yeriiiii <yerina98@gmail.com>
2023-01-12 13:51:12 +0900

    chore: tagitem

--- extension/src/components/TagItem.tsx
+++ extension/src/components/TagItem.tsx
@@ -3,19 +3,19 @@ import { tagsInFeedState } from "../states/atom";
 
 export function TagItem(props: any) {
   // console.log("여기는 태그아이템");
-  const setFeedTags = useSetRecoilState(tagsInFeedState);
+  // const setFeedTags = useSetRecoilState(tagsInFeedState);
 
-  const buttonClickHandler = () => {
-    newTag = {
-        <span>
-      <TagItem id={tag.tag_id} name={tag.tag_name}></TagItem>
-        </span>
-}
-    setFeedTags((oldTags: any) => [...oldTags, newTag]);
-  };
+//   const buttonClickHandler = () => {
+//     newTag = {
+//         <span>
+//       <TagItem id={tag.tag_id} name={tag.tag_name}></TagItem>
+//         </span>
+// }
+//     setFeedTags((oldTags: any) => [...oldTags, newTag]);
+//   };
 
   return (
-    <button onClick={buttonClickHandler} className="mt-1">
+    <button className="mt-1">
       <span className="inline-flex items-center mr-1 px-1.5 py-0.2 rounded-full text-xs font-medium bg-sky-100 text-sky-800 hover:bg-sky-300">
         #{props.name}
       </span>
