0fdc4804d0c0f6fa363dbfe35172f5a0e6d88445
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-01-17 00:39:28 +0900

    fix:tag

--- client/src/components/Tags/TagItem/TagItem.tsx
+++ client/src/components/Tags/TagItem/TagItem.tsx
@@ -22,7 +22,7 @@ export function TagItem(props: any) {
       </span>
     ) : (
       <span className="inline-flex items-center mr-2 px-3 py-0.5 rounded-full text-sm font-medium bg-sky-100 text-sky-800 hover:bg-sky-300 hover:scale-95">
-        {props.name}
+        #{props.name}
       </span>
     );
   const tagClickHandler = () => {
