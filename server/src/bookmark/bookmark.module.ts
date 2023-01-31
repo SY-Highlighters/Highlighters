f9de8353203cc5d544e53917f5c74a49338a24c9
Gilin3000 <lotus0110@nate.com>
2023-01-31 23:08:12 +0900

    feat : generatedFeed 함수로 condition

--- server/src/bookmark/bookmark.module.ts
+++ server/src/bookmark/bookmark.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
+import { FeedService } from 'src/feed/feed.service';
+import { ElasticsearchService } from 'src/repository/connection';
+import { TagService } from 'src/tag/tag.service';
 import { BookmarkController } from './bookmark.controller';
 import { BookmarkService } from './bookmark.service';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    CacheModule.register({
+      store: redisStore,
+      url: process.env.REDIS_URL,
+    }),
+  ],
   controllers: [BookmarkController],
-  providers: [BookmarkService],
+  providers: [BookmarkService, FeedService, TagService, ElasticsearchService],
 })
 export class BookmarkModule {}
--- server/src/bookmark/bookmark.service.ts
+++ server/src/bookmark/bookmark.service.ts
@@ -1,3 +1,4 @@
+import { FeedService } from 'src/feed/feed.service';
 import { Injectable, HttpException } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/repository/prisma.service';
@@ -6,7 +7,10 @@ import { RequestBookmarkDto } from './dto/bookmark.dto';
 
 @Injectable()
 export class BookmarkService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly feedService: FeedService,
+  ) {}
 
   async createBookmark(
     requestBookmarkDto: RequestBookmarkDto,
@@ -54,74 +58,6 @@ export class BookmarkService {
   }
 
   async getBookmark(page: number, take: number, user: User) {
-    try {
-      const count = await this.prismaService.feed.count({
-        where: {
-          bookmark: {
-            some: {
-              user_email: user.email,
-            },
-          },
-        },
-      });
-      const feeds = await this.prismaService.feed.findMany({
-        where: {
-          bookmark: {
-            some: {
-              user_email: user.email,
-            },
-          },
-        },
-        orderBy: { updatedAt: 'desc' },
-        take: take,
-        skip: take * (page - 1),
-        include: {
-          highlight: {
-            include: {
-              user: {
-                select: {
-                  nickname: true,
-                  image: true,
-                },
-              },
-            },
-            orderBy: [
-              {
-                user_email: 'asc',
-              },
-              {
-                createdAt: 'asc',
-              },
-            ],
-          },
-          tag: true,
-          og: true,
-          user: {
-            select: {
-              nickname: true,
-              image: true,
-            },
-          },
-          comment: {
-            orderBy: { createdAt: 'desc' },
-          },
-          bookmark: {
-            where: {
-              user_email: user.email,
-            },
-            select: {
-              id: true,
-            },
-          },
-        },
-      });
-      return {
-        currentPage: page,
-        totalPage: Math.ceil(count / take),
-        feeds,
-      };
-    } catch (e) {
-      throw new HttpException('Internal Server Error', 500);
-    }
+    return this.feedService.generatedFeed(page, take, user, 2);
   }
 }
