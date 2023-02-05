4007db0362da8f8b0e5a73f8e09c192fc55892a3
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-02-06 01:55:55 +0900

    fix: sighup enter

--- client/src/components/Intro/SignUp.tsx
+++ client/src/components/Intro/SignUp.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Fragment } from "react";
+import { Fragment,useState } from "react";
 import { useSetRecoilState, useRecoilState } from "recoil";
 import { logModalVisble, sighUpCheck, userInfoState } from "../../states/atom";
 import { useCookies } from "react-cookie";
@@ -7,18 +7,36 @@ import Swal from "sweetalert2";
 
 export default function SignUp() {
   const [sighch, setSignUp] = useRecoilState(sighUpCheck);
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const nickNameHandler = (e: any) => {
+    setNickname(e.target.value);
+  };
+  const emailHandler = (e: any) => {
+    setEmail(e.target.value);
+  };
+  const passwordHandler = (e: any) => {
+    setPassword(e.target.value);
+  };
+  
   // 비밀번호와 비밀 번호 확인 일치 여부
   // const [passwordCheck, setPasswordCheck] = useState(false);
   // const [passwordError, setPasswordError] = useState(false);
-
+  const onCheckEnter = (e: any) => {
+    if (e.key === "Enter") {
+      formSubmitHandler(e);
+    }
+  };
   // 폼 데이터 받기 -> 이건 건들지 않는게 낫겠다.
   const formSubmitHandler = async (event: any) => {
     event.preventDefault();
     // console.log(event);
     // form에서 email, password 받아오기
-    const nickname = event.target[0].value;
-    const email = event.target[1].value;
-    const password = event.target[2].value;
+    // const nickname = event.target[0].value;
+    // const email = event.target[1].value;
+    // const password = event.target[2].value;
     await axios
       .post(`${process.env.REACT_APP_HOST}/api/auth/signup`, {
         nickname: nickname,
@@ -54,7 +72,7 @@ export default function SignUp() {
           </a>
         </div>
         <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
-          <form onSubmit={formSubmitHandler}>
+          <form onSubmit={formSubmitHandler} onKeyDown={onCheckEnter}>
             <div>
               <label
                 htmlFor="name"
@@ -64,6 +82,7 @@ export default function SignUp() {
               </label>
               <div className="flex flex-col items-start">
                 <input
+                  onChange={nickNameHandler}
                   type="text"
                   name="name"
                   minLength={4}
@@ -80,6 +99,7 @@ export default function SignUp() {
               </label>
               <div className="flex flex-col items-start">
                 <input
+                  onChange={emailHandler}
                   type="email"
                   name="email"
                   className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
@@ -110,6 +130,7 @@ export default function SignUp() {
               </label>
               <div className="flex flex-col items-start">
                 <input
+                  onChange={passwordHandler}
                   type="password"
                   name="password_confirmation"
                   className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
@@ -117,11 +138,12 @@ export default function SignUp() {
               </div>
             </div>
             <div className="flex items-center justify-end mt-4">
-              <button onClick={signUpChangeHandler}>
-                <p className="text-sm text-gray-600 underline hover:text-gray-900">
-                  Already registered?
-                </p>
-              </button>
+              <p
+                onClick={signUpChangeHandler}
+                className="text-sm text-gray-600 underline hover:text-gray-900"
+              >
+                Already registered?
+              </p>
               <button
                 type="submit"
                 className="inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md bg-sky-900 active:bg-sky-900 false"
