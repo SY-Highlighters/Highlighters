6d041abba835fe340df102a1eb4121a4636e5685
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-02-25 19:43:06 +0900

    fix:google login ID hide

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 // React
-import { useEffect, useState, Fragment } from "react";
 import { useCookies } from "react-cookie";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-
 // Layout
 //  header section
 import Header from "./components/Layout/Header";
@@ -16,31 +14,16 @@ import LoginModal from "./components/Intro/LoginModal";
 // state management section
 import { useRecoilValue } from "recoil";
 import { logModalVisble, userInfoState } from "./states/atom";
-const clientId =
-  "1051615347268-qio4ne1nai8flq7felb5h0relc1lcp0b.apps.googleusercontent.com";
+
 function App() {
   const loginModalState = useRecoilValue(logModalVisble);
-  const [cookies, setCookie, removeCookie] = useCookies(["logCookie"]);
-  // 유저정보의 변화가 있을때만 리렌더링
-  // const userData = useRecoilValue(userInfo);
-  // const [localUser, setLocalUser] = useState(userData);
-  // useEffect(() => {
-  //   setLocalUser(userData);
-  // }, [userData]);
-  // const userData = useRecoilValue(userInfo);
+  const [cookies] = useCookies(["logCookie"]);
+
   // 로그인 상태를 확인해서 로그인 상태면 전체적으로 뷰 변경
   const header = !cookies.logCookie ? <LoginHeader /> : <Header />;
-  // // const logedMain = bookmarkOn ? (
-  // //   <AvailableFeeds></AvailableFeeds>
-  // // ) : (
-  // //   <AvailableBookmarks></AvailableBookmarks>
-  // // );
-  // (bookmarkOn && <AvailableFeeds></AvailableFeeds>)(
-  //   !bookmarkOn && <AvailableTags></AvailableTags>
-  // );
 
   return (
-    <GoogleOAuthProvider clientId={clientId}>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID!}>
       <div className="flex flex-col xl:h-screen">
         {/* header section */}
         {header}
